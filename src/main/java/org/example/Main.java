6d6e50b7687f1c864b64b04054e292dc927e4875
Guzel Ganieva <guzel95@list.ru>
2023-07-26 22:41:40 +0300

    added threads pull (ExecutorService)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Library library = new Library();
         Library.Producer producer = new Library.Producer(library);
         Library.Reader reader = new Library.Reader(library);
-        new Thread(reader).start();
-        new Thread(producer).start();
+        ExecutorService executor = Executors.newFixedThreadPool(8);
+        executor.execute(producer);
+        executor.execute(reader);
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        //new Thread(reader).start();
+        //new Thread(producer).start();
     }
 }
